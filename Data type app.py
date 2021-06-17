164fc449e9eaabcb303609c1448d8f7deb5a4ed4
F.thawin <thawin.fuf@gmail.com>
2021-06-17 15:55:23 +0700

    Create Data type app.py

---
+++ Data type app.py
@@ -0,0 +1,7 @@
+print("Welcome to MY world")
+print("-------------------")
+print("Iphone 11",1,25000,"THB -",1*25000,"THB")
+print("Iphone 12",1,40000,"THB -",1*40000,"THB")
+print("-------------------")
+print("Total",25000+40000,"THB")
+print("Thank you MY world")
